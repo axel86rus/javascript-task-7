4bd8c8b95030a58e00aa06e9fe1741cff7ba6c4f
axel86rus <lopuchaleksey@mail.ru>
2017-11-29 18:59:47 +0500

    one version

--- async.js
+++ async.js
@@ -1,13 +1,46 @@
+// import { resolve } from "dns";
+// import { ftruncateSync } from "fs";
+
 'use strict';
 
 exports.isStar = true;
 exports.runParallel = runParallel;
 
-/** Функция паралелльно запускает указанное число промисов
- * @param {Array} jobs – функции, которые возвращают промисы
- * @param {Number} parallelNum - число одновременно исполняющихся промисов
- * @param {Number} timeout - таймаут работы промиса
- */
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+    return new Promise(resolve => {
+        if (jobs === []) {
+            resolve([]);
+        }
+
+        let results = [];
+        let indexOfJob = 0;
+        let queue = jobs.slice(0, parallelNum);
+        let jobsIsWait = jobs.slice(parallelNum);
+        queue.map(job => start(job, indexOfJob++));
+
+        function getTranslate(job) {
+            return new Promise((resolveObj, rejectObj) => {
+                job().then(resolveObj, rejectObj);
+                setTimeout(rejectObj, timeout, new Error('Promise timeout'));
+            });
+        }
+
+        function start(job, indexResult) {
+            let finishResult = result => finish(result, indexResult);
+
+            return getTranslate(job)
+                .then(finishResult)
+                .catch(finishResult);
+        }
+
+        function finish(result, indexResult) {
+            results[indexResult] = result;
+
+            if (!jobsIsWait.length) {
+                resolve(results);
+            } else {
+                start(jobsIsWait.shift(), indexOfJob++);
+            }
+        }
+    });
 }
